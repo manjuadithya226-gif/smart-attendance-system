d25785c016c770a596d3d941754b0a1b49b5e6db
manjuadithya226-gif <manjuadithya226@gmail.com>
2026-04-14 20:47:29 +0530

    Refactored package structure to personal namespace

---
+++ src/main/java/com/exe/attendance/model/Student.java
@@ -0,0 +1,16 @@
+package com.example.attendance.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // getters and setters
+}
